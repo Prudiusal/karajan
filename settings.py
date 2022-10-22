a221943dcab9e2614e082b2cb164eb6f6f204730
ge6rgii <georgiibochorishvili@gmail.com>
2022-10-22 20:51:00 +0400

    typo

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ load_dotenv()
 SAMPLE_RATE = int(environ.get('SAMPLE_RATE', 44100))
 BUFFER_SIZE = int(environ.get('BUFFER_SIZE', 128))
 
-# Python on Windows will automatically handle forward slashes (/) properly;
-# or you can simple use '\\' instead.
+# Python on Windows will automatically handle forward slashes '/' properly;
+# or you can simply use '\\' instead.
 SERUM_PATH = environ.get('SERUM_PATH', 'C:/Program Files/Common Files/VST2/ValhallaSupermassive_x64.dll')
 VALHALLAFREQ_PATH = environ.get('VALHALLAFREQ_PATH', 'C:/vstplugins/ValhallaFreqEcho_x64.dll')
 VALHALLASUPER_PATH = environ.get('VALHALLASUPER_PATH', 'C:/Program Files/Common Files/VST2/ValhallaSupermassive_x64.dll')
