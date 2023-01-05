260042a203136390173afb68cb05a6e5ddc170a3
Aleksei_Prudius <prudius.av@proton.me>
2023-01-06 00:37:51 +0300

    One more file

--- Logic/RenderEngine.py
+++ Logic/RenderEngine.py
@@ -10,6 +10,9 @@ from pm_tests import check_length
 
 
 class RenderEngine(dawdreamer.RenderEngine):
+    """
+    Creates engine, stores tracks configuration for the style.
+    """
     def __init__(self, sample_rate=44100, buffer_size=128):
         self.sample_rate = sample_rate
         self.buffer_size = buffer_size
@@ -18,12 +21,18 @@ class RenderEngine(dawdreamer.RenderEngine):
         self.graph = []
 
     def create_tracks(self, style):
+        """
+        Creates the instances for each track of the song.
+        :param style: StyleConf object, contains the information of
+        each tracks in a style. For each track the plugins and presets
+        are noticed.
+        """
         for track_data in style.tracks:
             track_name = track_data['track_name']
             self.tracks[track_name] = Track(track_data,
                                             self.make_plugin_processor,
                                             self.make_add_processor)
-            self.tracks[track_name].construct()
+            self.tracks[track_name].construct()  # creates all vst plugins
 
     def construct_graph(self):
         adder_args = []
@@ -38,9 +47,7 @@ class RenderEngine(dawdreamer.RenderEngine):
         logger_render.debug(self.graph)
 
     def process_song(self, song_data):
-        # BPM = song_data.BPM
-        # song_length = song_data.SongLengthInSeconds
-        # output_path = song_data.OutputPath
+        ""
         logger_render.info('\nSong processing has started:')
         self.load_midi_into_tracks(song_data)
         logger_render.debug('Midi files are loaded\n')
@@ -50,8 +57,12 @@ class RenderEngine(dawdreamer.RenderEngine):
         self.rendered_output_path = song_data.OutputPath + \
             f'demo_{int(time.time())}.wav'
         logger_render.debug('Finished Rendering.')
+        self.save_audio()
 
     def save_audio(self):
+        """
+        Saves audio file ( now in a ./WAVs folder)
+        """
         audio = self.get_audio()
         logger_render.info(f'output path is {self.rendered_output_path}')
         wavfile.write(self.rendered_output_path, self.sample_rate,
@@ -60,30 +71,32 @@ class RenderEngine(dawdreamer.RenderEngine):
             logger_render.error('File is not saved')
 
     def load_midi_into_tracks(self, song_data):
+        """
+        Loads the midi files from the config to the appropriate synths.
+        """
         for params in song_data.Tracks:
             track_name = params['track_name']
             midi_path = params['midi_path']
-            if True:
-                print(song_data.SongLengthInSeconds)
-                print(check_length(midi_path))
-
             processors = self.tracks[track_name].processors
-            synth = next(iter(processors.values()))
+            synth = next(iter(processors.values()))  # take 1st
             synth.load_midi(midi_path)
-            logger_render.info(f'{synth=}')
+            logger_render.debug(f'{synth=}')
             logger_render.debug(f'midi_path {midi_path}')
 
     def preconfigure_renderer(self):
+        """Not used right now"""
         self.engine(self.sample_rate, self.buffer_size)
         self.engine.set_bpm(120.)
 
     def build_graph(self, serum_processor):
+        """Not used right now"""
         logger_render.debug('Building graph ...')
         graph = []
         graph.append((serum_processor, []))
         self.load_graph(graph)
 
     def render_to_file(self, song_data, sample_rate=44100):
+        """Not used right now"""
         logger_render.debug('Started Rendering.')
         self.render(song_data.length)
         audio = self.get_audio()
